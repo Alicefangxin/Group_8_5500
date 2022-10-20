046e5cec111b51e4e7a02edb110af7a98156657e
jingming <mary.j.ming@gmail.com>
2022-10-20 11:47:51 -0700

    40 create login page

---
+++ client/src/pages/login/login.jsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return(
+        <div>
+            <input type="text" placeholder="username" id="username"/>
+            <input type="password" placeholder="password" id="password"/>
+        </div>
+    )
+}
+
+export default Login;
