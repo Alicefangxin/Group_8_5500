0a4dbdbf874b2216db243f402999198796d037d3
jingming <mary.j.ming@gmail.com>
2022-10-20 12:41:16 -0700

    41 style login page

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,8 +1,14 @@
+import "./login.css"
+
 const Login = () => {
     return(
-        <div>
-            <input type="text" placeholder="username" id="username"/>
-            <input type="password" placeholder="password" id="password"/>
+        <div className="login">
+            <div className="lContainer">
+                <h4><b>Log in or create an account</b></h4>
+                <input type="text" placeholder="username" id="username" className="lInput"/>
+                <input type="password" placeholder="password" id="password" className="lInput"/>
+                <button className="lButton">Login</button>
+            </div>
         </div>
     )
 }
