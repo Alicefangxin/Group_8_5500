49aa560e53df76977c448ba866ee400dc5973fab
ylwn29 <ylwn29@gmail.com>
2022-11-30 12:46:28 -0800

    54 refactor frontend Register page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,38 +1,67 @@
-import "./register.css";
-import Navbar from "../../components/navbar/Navbar.jsx";
+import "./Register.css";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Register = () => {
 
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        email: undefined,
+        password: undefined,
+    });
+
+    const { loading, error, dispatch } = useContext(AuthContext);
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        //dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post("/auth/register", credentials);
+            //dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+            navigate("/")
+        } catch (err) {
+            //dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+        }
+    };
+
     return (
-        <div>
-            <Navbar registerShown = {false}/>
-            <div className="login">
-                <div className="lContainer">
-                    <input
-                        type="text"
-                        placeholder="username"
-                        id="username"
-                        className="lInput"
-                    />
-                    <input
-                        type="email"
-                        placeholder="email"
-                        id="email"
-                        className="lInput"
-                    />
-                    <input
-                        type="password"
-                        placeholder="password"
-                        id="password"
-                        className="lInput"
-                    />
-                    <button className="lButton">
-                        Register
-                    </button>
-                </div>
+        <div className="login">
+            <div className="lContainer">
+                <input
+                    type="text"
+                    placeholder="username"
+                    id="username"
+                    onChange={handleChange}
+                    className="lInput"
+                />
+                <input
+                    type="email"
+                    placeholder="email"
+                    id="email"
+                    onChange={handleChange}
+                    className="lInput"
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    id="password"
+                    onChange={handleChange}
+                    className="lInput"
+                />
+                <button disabled={loading} onClick={handleClick} className="lButton">
+                    Register
+                </button>
+                {error && <span>{error.message}</span>}
             </div>
         </div>
-
     );
 };
 export default Register;
