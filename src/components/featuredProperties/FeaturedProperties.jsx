d4faf0154dc7e28f1c36183570e43c4e85af63ba
Alicefangxin <alicefangxingu@gmail.com>
2022-12-05 10:02:39 -0800

    refactored client folder for netlify deployment

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,36 @@
+import "./featuredProperties.css";
+import useFetch from "../../hooks/useFetch.js";
+
+const FeaturedProperties = () => {
+
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");;
+
+  return (
+    <div className="fp">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                src={item.photos[0] ? item.photos[0]: "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"}
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+              {item.rating && <div className="fpRating">
+                <button>{item.rating}</button>
+                <span>Excellent</span>
+              </div>}
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FeaturedProperties;
